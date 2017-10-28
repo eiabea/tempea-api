8f2d99095a37eebbfc7dc337c7241e7c5575e5a6
eiabea <developer@eiabea.com>
2017-10-28 12:40:12 +0200

    implemented basic struture with closures added status route

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const bunyan = require('bunyan');
+const EXPRESS_PORT = parseInt(process.env.EXPRESS_PORT, 10) || 3000;
+const StatusRoute = require("./routes/v1/status.route");
+
+(async function(){
+  const log = bunyan.createLogger({name: 'tempea'});
+  log.info('Initializing routing module');
+
+  const app = express();
+
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(cors());
+  app.use(helmet());
+
+  const router = express.Router({mergeParams: true});
+
+  app.use(router);
+
+  app.use('/v1/status', StatusRoute);
+
+  log.info(`Starting tempea backend on port ${EXPRESS_PORT}`);
+  app.listen(EXPRESS_PORT, ()=> {
+    log.info(`tempea backend listening on port ${EXPRESS_PORT}`);
+  });
+})();
