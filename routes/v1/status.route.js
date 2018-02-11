e2ebfa146cc20c85cdb7a6a4b57cffc00509307f
eiabea <developer@eiabea.com>
2018-02-11 17:49:21 +0100

    fixed linting issues

--- routes/v1/status.route.js
+++ routes/v1/status.route.js
@@ -42,7 +42,8 @@ module.exports = (log, controller) => {
     });
   });
 
-  // router.post('/mode', controller.auth.authenticate(), controller.auth.authorize(), async (req, res) => {
+  // router.post('/mode', controller.auth.authenticate(),
+  //  controller.auth.authorize(), async (req, res) => {
   //   const { mode } = req.body;
   //   log.info({ mode }, 'Got mode request');
 
