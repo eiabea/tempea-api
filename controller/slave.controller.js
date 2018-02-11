aa9df9cbbc6ec6e2063846db7e27c52d04f082f3
eiabea <developer@eiabea.com>
2018-02-11 18:03:27 +0100

    using CI flag to return dummy slave data

--- controller/slave.controller.js
+++ controller/slave.controller.js
@@ -1,10 +1,21 @@
 const request = require('request');
 
-const { SLAVE_HOST, SLAVE_PORT, SLAVE_ENDPOINT } = process.env;
+const {
+  SLAVE_HOST, SLAVE_PORT, SLAVE_ENDPOINT, CI,
+} = process.env;
 
 module.exports = (log) => {
   const getData = async () => new Promise((resolve, reject) => {
-    request(`http://${SLAVE_HOST}:${SLAVE_PORT}${SLAVE_ENDPOINT}`, (error, response, body) => {
+    if (CI) {
+      return resolve({
+        success: true,
+        data: {
+          temp: 5,
+          hum: 50,
+        },
+      });
+    }
+    return request(`http://${SLAVE_HOST}:${SLAVE_PORT}${SLAVE_ENDPOINT}`, (error, response, body) => {
       if (error) {
         log.error({ error }, `Error getting data from slave ${SLAVE_HOST}`);
         return reject(error);
