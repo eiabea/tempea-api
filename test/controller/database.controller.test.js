729ab78caabca368d880a684d52ecc55035c119c
eiabea <developer@eiabea.com>
2019-01-06 17:46:21 +0100

    Mocked influx with proxyquire

--- test/controller/database.controller.test.js
+++ test/controller/database.controller.test.js
@@ -1,17 +1,10 @@
-require('../Helper').invalidateNodeCache();
+const mockedEnv = require('mocked-env');
 
 const log = require('null-logger');
+const { expect } = require('chai');
 const nock = require('nock');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
-const mockedInflux = function Influx(uri) {
-  this.createDatabase = async () => {
-    throw new Error("Mocked creation error")
-  };
-  this.schema = (one, two, three)=>{ }
-};
-
-process.env.CI = 'true';
 
 const INFLUX_HOST = process.env.INFLUX_HOST || 'influx';
 const INFLUX_PORT = process.env.INFLUX_PORT || 8086;
@@ -22,19 +15,45 @@ describe('Database Controller', () => {
   before(() => {
     nock(`http://${INFLUX_HOST}:${INFLUX_PORT}`)
       .post(/.*/g)
-      .times(4)
+      .times(8)
       .reply(200);
   });
 
   it('should ignore database creation error', async () => {
     const DBC = proxyquire('../../controller/database.controller', {
-      'influxdb-nodejs': mockedInflux,
+      'influxdb-nodejs': function Influx(uri) {
+        this.uri = uri;
+        this.createDatabase = sinon.stub().throws('Creation error');
+        this.schema = sinon.stub();
+      },
     });
 
     await DBC(log);
   });
 
-  it('should write measurement', async () => {
+  it('should fallback to default values if no env is set', async () => {
+
+    const restore = mockedEnv({
+      INFLUX_HOST: undefined,
+      INFLUX_PORT: undefined,
+      INFLUX_DB: undefined,
+    });
+
+    const DBC = proxyquire('../../controller/database.controller', {
+      'influxdb-nodejs': function Influx(uri) {
+        this.uri = uri;
+        expect(uri).to.eq('http://influx:8086/temp')
+        this.createDatabase = sinon.stub();
+        this.schema = sinon.stub();
+      },
+    });
+
+    await DBC(log);
+
+    restore();
+  });
+
+  it('should write measurement, heating off', async () => {
     const currentTemp = 21;
     const desiredTemp = 21;
     const heating = false;
@@ -48,10 +67,34 @@ describe('Database Controller', () => {
     await instance.writeMeasurement(currentTemp, desiredTemp, heating, slaveData);
   });
 
-  it('should write measurement without slave data', async () => {
+  it('should write measurement, heating on', async () => {
+    const currentTemp = 21;
+    const desiredTemp = 21;
+    const heating = true;
+    const slaveData = {
+      temp: 5,
+      hum: 50,
+    };
+
+    const instance = await DatabaseController(log);
+
+    await instance.writeMeasurement(currentTemp, desiredTemp, heating, slaveData);
+  });
+
+  it('should write measurement without slave data, heating off', async () => {
     const currentTemp = 21;
     const desiredTemp = 21;
     const heating = false;
+
+    const instance = await DatabaseController(log);
+
+    await instance.writeMeasurement(currentTemp, desiredTemp, heating);
+  });
+
+  it('should write measurement without slave data, heating on', async () => {
+    const currentTemp = 21;
+    const desiredTemp = 21;
+    const heating = true;
 
     const instance = await DatabaseController(log);
 
