db3889e098b1726e51bf770062b3ae1a1b89c1d8
eiabea <developer@eiabea.com>
2019-01-16 17:32:32 +0100

    Mocked away google-auth-library in calendar controller

--- test/controller/calendar.controller.test.js
+++ test/controller/calendar.controller.test.js
@@ -1,43 +1,34 @@
-require('../Helper').invalidateNodeCache();
-
-const { expect } = require('chai');
+const mockedEnv = require('mocked-env');
+const { assert, expect } = require('chai');
 const log = require('null-logger');
 const nock = require('nock');
 const moment = require('moment');
-
-const GOOGLE_CALENDAR_ID_VALID = '1337';
-const GOOGLE_CALENDAR_ID_NO_EVENTS = '404';
-const GOOGLE_CALENDAR_ID_NAN_SUMMERY = '400';
-const GOOGLE_CALENDAR_ID_OVER_MAX_TEMP = '9000';
-const GOOGLE_CALENDAR_ID_NOT_IN_RANGE = '004';
-const MAX_TEMP = 37;
-const MIN_TEMP = 17;
-
-process.env.CI = 'true';
-process.env.TOKEN_DIR = 'test/secrets';
-process.env.GOOGLE_SERVICE_ACCOUNT_JSON = 'tempea-mocked.json';
-
-process.env.GOOGLE_CALENDAR_ID = GOOGLE_CALENDAR_ID_VALID;
-
-// Invalidate require cache to create instance with new environment variables
-delete require.cache[require.resolve('../../controller/calendar.controller')];
+const sinon = require('sinon');
+const fs = require('fs');
+const { request } = require('gaxios');
+const proxyquire = require('proxyquire');
 
 const CacheController = require('../../controller/cache.controller')(log);
-const CalendarController = require('../../controller/calendar.controller')(log, CacheController);
 
 describe('Calendar Controller', () => {
-  before(() => {
+  let restore;
+  beforeEach(() => {
+    restore = mockedEnv({
+      GOOGLE_SERVICE_ACCOUNT_JSON: 'tempea-mocked.json',
+      GOOGLE_CALENDAR_ID: 'tempea-mocked',
+      TOKEN_DIR: 'test/secrets',
+      MAX_TEMP: '27',
+      MIN_TEMP: '15',
+    });
+  });
+
+  afterEach(() => {
+    restore();
+  });
+
+  it('should get desired temperature', async () => {
     nock('https://www.googleapis.com:443')
-      .post('/oauth2/v4/token')
-      .times(6)
-      .reply(200, {
-        access_token: '1/fFAGRNJru1FTz70BzhT3Zg',
-        expires_in: 3920,
-        token_type: 'Bearer',
-        refresh_token: '1/xEoDL4iW3cxlI7yDbSRFYNG01kVKM2C-259HOF2aQbI',
-      });
-    nock('https://www.googleapis.com:443')
-      .get(new RegExp(`/calendar/v3/calendars/${GOOGLE_CALENDAR_ID_VALID}/events/*`))
+      .get(new RegExp('/calendar/v3/calendars/tempea-mocked/events/*'))
       .reply(200, {
         items: [
           {
@@ -51,43 +42,53 @@ describe('Calendar Controller', () => {
           },
         ],
       });
+
+    const authorizeSpy = sinon.spy();
+
+    const CC = proxyquire('../../controller/calendar.controller', {
+      'google-auth-library': {
+        JWT: function JWT() {
+          this.authorize = authorizeSpy;
+          this.request = async opts => request(opts);
+        },
+      },
+    });
+
+    const desiredTemp = await CC(log, CacheController)
+      .getDesiredTemperature();
+
+    expect(desiredTemp).to.equal(18.4);
+    assert.isTrue(authorizeSpy.called);
+  });
+
+  it('should return MIN_TEMP [no events]', async () => {
     nock('https://www.googleapis.com:443')
-      .get(new RegExp(`/calendar/v3/calendars/${GOOGLE_CALENDAR_ID_NO_EVENTS}/events/*`))
+      .get(new RegExp('/calendar/v3/calendars/tempea-mocked/events/*'))
       .reply(200, {
         items: [],
       });
+
+    const authorizeSpy = sinon.spy();
+
+    const CC = proxyquire('../../controller/calendar.controller', {
+      'google-auth-library': {
+        JWT: function JWT() {
+          this.authorize = authorizeSpy;
+          this.request = async opts => request(opts);
+        },
+      },
+    });
+
+    const desiredTemp = await CC(log, CacheController)
+      .getDesiredTemperature();
+
+    expect(desiredTemp).to.equal(15);
+    assert.isTrue(authorizeSpy.called);
+  });
+
+  it('should return MIN_TEMP [event not in range]', async () => {
     nock('https://www.googleapis.com:443')
-      .get(new RegExp(`/calendar/v3/calendars/${GOOGLE_CALENDAR_ID_NAN_SUMMERY}/events/*`))
-      .reply(200, {
-        items: [
-          {
-            summary: 'wrongSummery',
-            start: {
-              dateTime: moment().subtract(1, 'days').toISOString(),
-            },
-            end: {
-              dateTime: moment().add(1, 'days').toISOString(),
-            },
-          },
-        ],
-      });
-    nock('https://www.googleapis.com:443')
-      .get(new RegExp(`/calendar/v3/calendars/${GOOGLE_CALENDAR_ID_OVER_MAX_TEMP}/events/*`))
-      .reply(200, {
-        items: [
-          {
-            summary: '49.2',
-            start: {
-              dateTime: moment().subtract(1, 'days').toISOString(),
-            },
-            end: {
-              dateTime: moment().add(1, 'days').toISOString(),
-            },
-          },
-        ],
-      });
-    nock('https://www.googleapis.com:443')
-      .get(new RegExp(`/calendar/v3/calendars/${GOOGLE_CALENDAR_ID_NOT_IN_RANGE}/events/*`))
+      .get(new RegExp('/calendar/v3/calendars/tempea-mocked/events/*'))
       .reply(200, {
         items: [
           {
@@ -101,82 +102,155 @@ describe('Calendar Controller', () => {
           },
         ],
       });
-  });
 
-  it('should get desired temperature', async () => {
-    const desiredTemp = await CalendarController.getDesiredTemperature();
+    const authorizeSpy = sinon.spy();
 
-    expect(desiredTemp).to.equal(18.4);
-  });
+    const CC = proxyquire('../../controller/calendar.controller', {
+      'google-auth-library': {
+        JWT: function JWT() {
+          this.authorize = authorizeSpy;
+          this.request = async opts => request(opts);
+        },
+      },
+    });
 
-  it('should return MIN_TEMP [no events]', async () => {
-    process.env.GOOGLE_CALENDAR_ID = GOOGLE_CALENDAR_ID_NO_EVENTS;
-    process.env.MIN_TEMP = MIN_TEMP;
-
-    // Invalidate require cache to create instance with new environment variables
-    delete require.cache[require.resolve('../../controller/calendar.controller')];
-
-    // eslint-disable-next-line global-require
-    const desiredTemp = await require('../../controller/calendar.controller')(log, CacheController)
+    const desiredTemp = await CC(log, CacheController)
       .getDesiredTemperature();
 
-    expect(desiredTemp).to.equal(MIN_TEMP);
-  });
-
-  it('should return MIN_TEMP [event not in range]', async () => {
-    process.env.GOOGLE_CALENDAR_ID = GOOGLE_CALENDAR_ID_NOT_IN_RANGE;
-
-    // Invalidate require cache to create instance with new environment variables
-    delete require.cache[require.resolve('../../controller/calendar.controller')];
-
-    // eslint-disable-next-line global-require
-    const desiredTemp = await require('../../controller/calendar.controller')(log, CacheController)
-      .getDesiredTemperature();
-
-    expect(desiredTemp).to.equal(MIN_TEMP);
+    expect(desiredTemp).to.equal(15);
+    assert.isTrue(authorizeSpy.called);
   });
 
   it('should return MIN_TEMP [wrong summery]', async () => {
-    process.env.GOOGLE_CALENDAR_ID = GOOGLE_CALENDAR_ID_NAN_SUMMERY;
-    process.env.MIN_TEMP = MIN_TEMP;
+    nock('https://www.googleapis.com:443')
+      .get(new RegExp('/calendar/v3/calendars/tempea-mocked/events/*'))
+      .reply(200, {
+        items: [
+          {
+            summary: 'wrongSummery',
+            start: {
+              dateTime: moment().subtract(1, 'days').toISOString(),
+            },
+            end: {
+              dateTime: moment().add(1, 'days').toISOString(),
+            },
+          },
+        ],
+      });
 
-    // Invalidate require cache to create instance with new environment variables
-    delete require.cache[require.resolve('../../controller/calendar.controller')];
+    const authorizeSpy = sinon.spy();
 
-    // eslint-disable-next-line global-require
-    const desiredTemp = await require('../../controller/calendar.controller')(log, CacheController)
+    const CC = proxyquire('../../controller/calendar.controller', {
+      'google-auth-library': {
+        JWT: function JWT() {
+          this.authorize = authorizeSpy;
+          this.request = async opts => request(opts);
+        },
+      },
+    });
+
+    const desiredTemp = await CC(log, CacheController)
       .getDesiredTemperature();
 
-    expect(desiredTemp).to.equal(MIN_TEMP);
+    expect(desiredTemp).to.equal(15);
+    assert.isTrue(authorizeSpy.called);
   });
 
   it('should return MAX_TEMP [desired temp too high]', async () => {
-    process.env.GOOGLE_CALENDAR_ID = GOOGLE_CALENDAR_ID_OVER_MAX_TEMP;
-    process.env.MAX_TEMP = MAX_TEMP;
-    process.env.MIN_TEMP = MIN_TEMP;
+    nock('https://www.googleapis.com:443')
+      .get(new RegExp('/calendar/v3/calendars/tempea-mocked/events/*'))
+      .reply(200, {
+        items: [
+          {
+            summary: '49.2',
+            start: {
+              dateTime: moment().subtract(1, 'days').toISOString(),
+            },
+            end: {
+              dateTime: moment().add(1, 'days').toISOString(),
+            },
+          },
+        ],
+      });
 
-    // Invalidate require cache to create instance with new environment variables
-    delete require.cache[require.resolve('../../controller/calendar.controller')];
+    const authorizeSpy = sinon.spy();
 
-    // eslint-disable-next-line global-require
-    const desiredTemp = await require('../../controller/calendar.controller')(log, CacheController)
+    const CC = proxyquire('../../controller/calendar.controller', {
+      'google-auth-library': {
+        JWT: function JWT() {
+          this.authorize = authorizeSpy;
+          this.request = async opts => request(opts);
+        },
+      },
+    });
+
+    const desiredTemp = await CC(log, CacheController)
       .getDesiredTemperature();
 
-    expect(desiredTemp).to.equal(MAX_TEMP);
+    expect(desiredTemp).to.equal(27);
+    assert.isTrue(authorizeSpy.called);
   });
 
-  it('should through [invalid service json]', async () => {
-    process.env.GOOGLE_SERVICE_ACCOUNT_JSON = 'invalid.json';
+  it('should throw [invalid service json]', async () => {
+    restore = mockedEnv({
+      GOOGLE_SERVICE_ACCOUNT_JSON: 'invalid.json',
+      GOOGLE_CALENDAR_ID: 'tempea-mocked',
+      TOKEN_DIR: 'test/secrets',
+      MAX_TEMP: '27',
+      MIN_TEMP: '15',
+    });
 
-    // Invalidate require cache to create instance with new environment variables
-    delete require.cache[require.resolve('../../controller/calendar.controller')];
+    const CC = proxyquire('../../controller/calendar.controller', {
+      'google-auth-library': {
+        JWT: function JWT(obj) {
+          fs.readFileSync(obj.keyFile);
+        },
+      },
+    });
 
     try {
-      // eslint-disable-next-line global-require
-      await require('../../controller/calendar.controller')(log, CacheController)
-        .getDesiredTemperature();
+      await CC(log, CacheController).getDesiredTemperature();
     } catch (err) {
       expect(err.code).to.equal('ENOENT');
+    }
+  });
+
+  it('should throw [calendar.event.list]', async () => {
+    restore = mockedEnv({
+      GOOGLE_SERVICE_ACCOUNT_JSON: 'tempea-mocked.json',
+      GOOGLE_CALENDAR_ID: 'tempea-mocked',
+      TOKEN_DIR: 'test/secrets',
+      MAX_TEMP: '27',
+      MIN_TEMP: '15',
+    });
+
+    const authorizeSpy = sinon.spy();
+    const listStub = sinon.stub().callsArgWith(1, new Error(), 0);
+
+    const CC = proxyquire('../../controller/calendar.controller', {
+      googleapis: {
+        google: {
+          calendar() {
+            return {
+              events: {
+                list: listStub,
+              },
+            };
+          },
+        },
+      },
+      'google-auth-library': {
+        JWT: function JWT() {
+          this.authorize = authorizeSpy;
+          this.request = async opts => request(opts);
+        },
+      },
+    });
+
+    try {
+      await CC(log, CacheController).getDesiredTemperature();
+    } catch (err) {
+      assert.isDefined(err);
     }
   });
 });
